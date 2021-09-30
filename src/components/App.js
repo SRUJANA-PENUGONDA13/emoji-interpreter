b9e75290e154fcb1cf8eef3eb6cc3a9d0479e480
Srujana Penugonda <penugondasrujana3@gmail.com>
2021-09-30 10:22:51 +0530

    Refactor - Updated Number of emoji's

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,60 @@
 import { useState } from "react";
-import React from 'react';
+import React from "react";
 
-const App = ()=>
-{
-    var emojiDict = {
-        "😊" : "Smiling",
-        "😳" : "disbelief",
-        "😔" : "sad",
-        "🥡" : "takeout box",
-        "❤️" : "love",
-        "😑" : "annoyance"
+const App = () => {
+  var emojiDict = {
+    "😊": "Smiling",
+    "😳": "disbelief",
+    "😔": "sad",
+    "🥡": "takeout box",
+    "❤️": "love",
+    "😑": "annoyance",
+    "🥰": "Smiling Face with Hearts",
+    "😘": "Face Blowing a Kiss",
+    "😋": "Face Savoring Food",
+    "🤪": "Zany Face",
+  };
+  const emojis = Object.keys(emojiDict);
+  const [outputText, setOutputText] = useState("Translation Will appear here.");
+  const [emojiOutput, setemojiOutput] = useState("");
+  const emojiClickHandler = (emoji) => {
+    if (emoji in emojiDict) {
+      setOutputText(emojiDict[emoji]);
+    } else {
+      setOutputText("Failure to recognise this emoji");
+    }
+  };
+  const searchEmojiHandler = (emoji) => {
+    setemojiOutput(emoji);
+    emojiClickHandler(emoji);
+  };
 
-    }
-    const emojis = Object.keys(emojiDict);
-    const [ outputText , setOutputText ] = useState("Translation Will appear here.")
-    const [ emojiOutput, setemojiOutput ]  = useState("")
-    const emojiClickHandler = (emoji)=>
-    {
-        if( emoji in emojiDict)
-        {
-            setOutputText(emojiDict[emoji]);
-        }
-        else
-        {
-            setOutputText("Failure to recognise this emoji")
-        }
-    }
-    const searchEmojiHandler = (emoji) =>
-    {
-        setemojiOutput(emoji)
-        emojiClickHandler(emoji)
-    }
-   
-    return (
-        <div className="container">
-            <div className="heading">
-                <h1>inside outttttt</h1>
-            </div>
-            <div className="input">
-                <input placeholder="Search your emoji" onChange= {(event) => { searchEmojiHandler(event.target.value) }}></input>
-            </div>
-            <div className="output">
-                <div className="displayResult">
-                    <h2>{emojiOutput}</h2>
-                    <h3>{outputText}</h3>
-                </div>
-                <div className="emojis">
-                {
-                    emojis.map((emoji) => (
-                    <span onClick={()=>emojiClickHandler(emoji)}>
-                        {emoji}
-                    </span>
-                    ))
-                }
-                </div>
-            </div>
-            
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>inside outttttt</h1>
+      </div>
+      <div className="input">
+        <input
+          placeholder="Search your emoji"
+          onChange={(event) => {
+            searchEmojiHandler(event.target.value);
+          }}
+        ></input>
+      </div>
+      <div className="output">
+        <div className="displayResult">
+          <h2>{emojiOutput}</h2>
+          <h3>{outputText}</h3>
         </div>
-    
-    )
-    ;
-}
+        <div className="emojis">
+          {emojis.map((emoji) => (
+            <span onClick={() => emojiClickHandler(emoji)}>{emoji}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
